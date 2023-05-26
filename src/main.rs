bfa21df930130ca1d46af5035517e2a226d829bd
ayushmishra <mail81mitthu@gmail.com>
2023-05-26 21:50:47 +0530

    hash should be decripted now

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use sha2::{Digest, Sha256};
+use std::{
+    env,
+    fs::File,
+    io::{BufRead, BufReader},
+    process::exit,
+};
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let wanted_hash = &args[1];
+    let password_file = "src/passlist.txt";
+    let mut attempts = 1;
+
+    println!("Cracking {}\n", wanted_hash);
+
+    let password_list = File::open(password_file).unwrap();
+    let reader = BufReader::new(password_list);
+
+    for line in reader.lines() {
+        let line = line.unwrap();
+        let password = line.trim().to_owned().into_bytes();
+        let password_hash = format!("{:x}", Sha256::digest(&password));
+
+        println!("[{}] {} == {}", attempts, std::str::from_utf8(&password).unwrap(), password_hash);
+
+        if &password_hash == wanted_hash {
+            println!("Found after {} Password = {}\n", attempts,password_hash);
+            exit(0);
+        }
+        attempts += 1;
+    }
+    println!("No hash found");
+
 }
